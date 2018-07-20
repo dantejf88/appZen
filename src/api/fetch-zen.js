40c68c1cf0377a4703119e17748dbeb3232a4747
dantejf88 <dantejf88@gmail.com>
2018-07-20 15:29:15 -0300

    Sin Request-Promise

--- src/api/fetch-zen.js
+++ src/api/fetch-zen.js
@@ -1,19 +1,16 @@
-import rp from 'request-promise'
-
 var getZen = () => {
-  let request = {
-    method: 'GET',
-    url: 'https://api.github.com/zen',
-    headers: {
-      "Authorization": "token 04b1f2e029f158681f795bdd9d2cb9584bbe5478"
-    }
-  }
-  return rp(request)
+  let urlApi = "https://api.github.com/zen"
+  return fetch(urlApi,
+    {
+      headers: {
+        "Authorization": "token 04b1f2e029f158681f795bdd9d2cb9584bbe5478"
+      }
+    })
     .then((response) => {
-      return response
+      return response.text()
     })
     .catch((error) => {
       throw new Error('GET request failed', error);
     })
-}
-export default getZen
+  }
+  export default getZen
